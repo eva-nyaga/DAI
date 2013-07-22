c90684e414e23a665f60b32a05a0184e4cc9eb2c
Evalyne Mukii Nyaga <evalynemukii@gmail.com>
2013-07-22 14:09:34 +0300

    First commit after initialising the repository

---
+++ db_config.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * All database connection variables
+ */
+
+define('DB_USER', "root"); // db user
+define('DB_PASSWORD', "eva1995"); // db password (mention your db password here)
+define('DB_DATABASE', "DAI"); // database name
+define('DB_SERVER', "localhost"); // db server
+?>
